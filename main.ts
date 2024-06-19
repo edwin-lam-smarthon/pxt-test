ed040c9fda8b1481c97b1a5eea4ac1bb521f2b8e
edwin-lam-smarthon <edwin.lam.smarthon@gmail.com>
2024-06-19 09:33:48 +0800

    Update pxt.json, main.blocks, main.ts, sensor.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ led.enable(false)
 OLED.init(128, 64)
 basic.forever(function () {
     OLED.clear()
-    OLED.writeStringNewLine("PH Value: " + AdvancedModule.readPhNumber(AnalogPin.P0, 0))
+    OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, 3.4))
     basic.pause(2000)
 })
--- sensor.ts
+++ sensor.ts
@@ -1,22 +1,27 @@
 namespace AdvancedModule {
-let ph_value_number = 0
-//% blockId="readPHNumber"
-//% block="Read PH value (x100) pin %ports| offset %offset"
-//% weight=70 group="Water"
-export function readPhNumber(ports: AnalogPin, offset: number): number {
+    let ph_value_number = 0
+    //% blockId="readPHNumber"
+    //% block="Read PH value (x100) pin %ports| offset %offset"
+    //% weight=70 group="Water"
+    export function readPhNumber(ports: AnalogPin, offset: number): number {
 
-    let temp = 0;
-    temp = ports
-    let sensorarray: number[] = []
-    for (let i = 0; i < 10; i++) {
-        sensorarray.push(pins.analogReadPin(temp))
-        basic.pause(10)
+        let temp = 0;
+        temp = ports
+        let sensorarray: number[] = []
+        for (let i = 0; i < 10; i++) {
+            sensorarray.push(pins.analogReadPin(temp))
+            basic.pause(10)
+        }
+        sensorarray.sort((n1, n2) => n1 - n2);
+        for (let value of sensorarray) {
+            serial.writeLine(value.toString())
+        }
+        let Averageval =0
+        for (let i = 0; i < 10; i++) {
+            Averageval += sensorarray[i]
+        }
+        Averageval = Averageval/10
+        ph_value_number = (Averageval * 6 / 1023) + offset
+        return ph_value_number
     }
-    sensorarray.sort((n1, n2) => n1 - n2);
-    for (let value of sensorarray) {
-        serial.writeLine(value.toString())
-    }
-    ph_value_number = (sensorarray[5] * 5 * 10 * 35 / 1024) + offset
-    return ph_value_number
-}
 }

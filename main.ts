28259b52a83fad5455d8143eda75582343f47296
edwin-lam-smarthon <edwin.lam.smarthon@gmail.com>
2024-06-19 09:39:04 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ led.enable(false)
 OLED.init(128, 64)
 basic.forever(function () {
     OLED.clear()
-    OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, 3.4))
+    OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, 4.1))
     basic.pause(2000)
 })

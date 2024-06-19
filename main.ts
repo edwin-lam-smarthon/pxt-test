847666f54241c6883e25a220496b1baa63973510
edwin-lam-smarthon <edwin.lam.smarthon@gmail.com>
2024-06-19 15:31:49 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+let ph = 0
 led.enable(false)
 OLED.init(128, 64)
 basic.forever(function () {
     OLED.clear()
-    OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, 3.6))
+    OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, 4.3))
+    ph = AdvancedModule.readPhNumber(AnalogPin.P0, 0)
+    WiFiIoT.sendThingspeak(
+    "EKD278W78BT094U0",
+    ph
+    )
     basic.pause(2000)
 })

907b67f946fc4013ffed1807a3b906093cfac1de
edwin-lam-smarthon <edwin.lam.smarthon@gmail.com>
2024-06-19 15:39:37 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
-WiFiIoT.on_thingspeak_conn(function (Status, Error_code) {
-    OLED.clear()
-    OLED.writeStringNewLine("Status: " + Status)
-    OLED.writeStringNewLine("Error:" + Error_code)
-})
+let offset = 0
 let ph = 0
 led.enable(false)
 OLED.init(128, 64)
 WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
 WiFiIoT.setWifi("smarthon", "12345678")
 basic.forever(function () {
-    OLED.clear()
-    OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, 4.3))
-    ph = AdvancedModule.readPhNumber(AnalogPin.P0, 0)
-    WiFiIoT.sendThingspeak(
-    "EKD278W78BT094U0",
-    ph
-    )
-    basic.pause(2000)
+    if (WiFiIoT.is_wifi_connect()) {
+        OLED.clear()
+        OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, 4.4))
+        ph = AdvancedModule.readPhNumber(AnalogPin.P0, 0)
+        offset = 4.4
+        WiFiIoT.sendThingspeak(
+        "EKD278W78BT094U0",
+        ph + offset
+        )
+        basic.pause(2000)
+    }
 })

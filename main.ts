8b08ac0fa0fd3521fdc1b1786179659eb87eac44
edwin-lam-smarthon <edwin.lam.smarthon@gmail.com>
2024-06-25 17:55:00 +0800

    Update main.blocks, main.ts, sensor.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,8 @@
-let offset = 0
-let ph = 0
 led.enable(false)
 OLED.init(128, 64)
-WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
-WiFiIoT.setWifi("smarthon", "12345678")
 basic.forever(function () {
-    if (WiFiIoT.is_wifi_connect()) {
-        OLED.clear()
-        OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, 5.3))
-        ph = AdvancedModule.readPhNumber(AnalogPin.P0, 0)
-        offset = 5.3
-        WiFiIoT.sendThingspeak(
-        "EKD278W78BT094U0",
-        ph + offset
-        )
-        basic.pause(2000)
-    }
+    OLED.clear()
+    OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P0, -2.5))
+    OLED.writeStringNewLine("PH: " + AdvancedModule.readPhNumber(AnalogPin.P1, -2.5))
+    basic.pause(2000)
 })
--- sensor.ts
+++ sensor.ts
@@ -1,27 +1,24 @@
 namespace AdvancedModule {
     let ph_value_number = 0
     //% blockId="readPHNumber"
-    //% block="Read PH value (x100) pin %ports| offset %offset"
+    //% block="Read PH value pin %ports| offset %offset"
     //% weight=70 group="Water"
     export function readPhNumber(ports: AnalogPin, offset: number): number {
 
         let temp = 0;
         temp = ports
         let sensorarray: number[] = []
+        let avgValue: number = 0;
         for (let i = 0; i < 10; i++) {
             sensorarray.push(pins.analogReadPin(temp))
             basic.pause(10)
         }
         sensorarray.sort((n1, n2) => n1 - n2);
-        for (let value of sensorarray) {
-            serial.writeLine(value.toString())
+        for (let i = 2; i < 8; i++) {
+            avgValue += sensorarray[i];
         }
-        let Averageval =0
-        for (let i = 0; i < 10; i++) {
-            Averageval += sensorarray[i]
-        }
-        Averageval = Averageval/10
-        ph_value_number = (Averageval * 5 / 1023) + offset
-        return ph_value_number
+
+        ph_value_number = (avgValue/ 6 * 5 / 1023)* 3.5 + offset
+        return ph_value_number * 1.00
     }
 }
